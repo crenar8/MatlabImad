2f52bd7c29637cdf442075d8a3d30dcd34f751e3
Fededgs <32876183+Fededgs@users.noreply.github.com>
2018-06-06 21:26:43 +0200

    stepwise lin e con poly55

--- PortataAria3.m
+++ PortataAria3.m
@@ -6,7 +6,7 @@ close all
 
 
 % Caricamento dei dati di giri, portata e pressione
-load("portata_aria2.mat");
+load('portata_aria2.mat');
 
 % Normalizzo le variabili
 girinorm = giri / norm(giri);
@@ -17,7 +17,7 @@ portatanorm = portata / norm(portata);
 % Guardo un primo plot
 figure(1)
 scatter3(girinorm, pressionenorm, portatanorm)
-title("Scatter Plot")
+title('Scatter Plot')
 xlabel('giri')
 ylabel('pressione')
 zlabel('portata')
@@ -29,16 +29,30 @@ pause
 
 X = [girinorm(trainInd), pressionenorm(trainInd)]; %utilizzo gli indici di girinorm selezionati da trainInd
 
-mdl = stepwiselm(X, portatanorm(trainInd))
+mdl_lin= stepwiselm(X, portatanorm(trainInd),'linear')
+mdl_poly=stepwiselm(X,portatanorm(trainInd),'poly55')
 
 %vettori di test
 X=linspace(min(girinorm),max(girinorm),10);
 Y=linspace(min(girinorm),max(girinorm),10);
 [X,Y]=meshgrid(X,Y);
-Zlin=feval(mdl,X,Y);
+Zlin=feval(mdl_lin,X,Y);
+Zpoly=feval(mdl_poly,X,Y);
+pause
+figure(2)
 surf(X,Y,reshape(Zlin,size(X)))
+title('Stepwise regression lin')
+pause
+figure(3)
+surf(X,Y,reshape(Zpoly,size(X)))
+title('Stepwise regression poly max 5')
 
 
+% con stepwise regressione e anche i polinomi con phi io stimo il modello. nel secondo 
+% caso la struttura del modello ce l ho già, nel primo caso lo trova il procedimento(anche i parametri??)
+% con le varie validazione vedo quale è il milgiore.
+% NB vedi: http://sisdin.unipv.it/labsisdin/teaching/courses/imadlt/files/IdentificazioneParametrica4.pdf
+% http://sisdin.unipv.it/labsisdin/teaching/courses/imadlt/files/IdentificazioneParametrica3.pdf
 
 %ESERCIZIO
 %phi
